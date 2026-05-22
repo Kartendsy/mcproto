bb7f8ccdd14265bb7dff32248f4ce1655c261390
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2026-05-23 00:24:04 +0700

    Update writer.go

--- mcbin/writer.go
+++ mcbin/writer.go
@@ -1,127 +1,104 @@
-package bin
+package packet
 
 import (
-	"bytes"
 	"encoding/binary"
 	"math"
-	"sync"
 )
 
 type Writer struct {
-	buf *bytes.Buffer
-}
-
-var bufferPool = sync.Pool{
-	New: func() any {
-		return &Writer{
-			buf: new(bytes.Buffer),
-		}
-	},
+	buf []byte
 }
 
 func NewWriter() *Writer {
-	w := bufferPool.Get().(*Writer)
-	w.buf.Reset()
+	return &Writer{buf: make([]byte, 0)}
+}
+
+// Variable Length
+
+func (w *Writer) VarInt(v int32) *Writer {
+	u := uint32(v)
+	for u >= 0x80 {
+		w.buf = append(w.buf, byte(u&0x7F|0x80))
+		u >>= 7
+	}
+	w.buf = append(w.buf, byte(u))
 	return w
 }
 
-func ReleaseWriter(w *Writer) {
-	// Reset buffer sebelum dikembalikan agar memori bisa digunakan ulang
-	w.buf.Reset()
-	bufferPool.Put(w)
+func (w *Writer) String(v string) *Writer {
+	w.VarInt(int32(len(v)))
+	w.buf = append(w.buf, v...)
+	return w
 }
 
-func (w *Writer) Bytes() []byte {
-	return w.buf.Bytes()
+// Fixed Lenght Signed
+
+func (w *Writer) Byte(v byte) *Writer {
+	w.buf = append(w.buf, v)
+	return w
 }
 
-func (w *Writer) WriteByte(val byte) error {
-	return w.buf.WriteByte(val)
+func (w *Writer) Short(v int16) *Writer {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, uint16(v))
+	w.buf = append(w.buf, b...)
+	return w
 }
 
-func (w *Writer) WriteVarInt(val int32) error {
-	uval := uint32(val)
-	for {
-		temp := byte(uval & 0x7F)
-		uval >>= 7
-		if uval != 0 {
-			temp |= 0x80
-		}
-		if err := w.buf.WriteByte(temp); err != nil {
-			return err
-		}
-		if uval == 0 {
-			break
-		}
-	}
-	return nil
+func (w *Writer) Int(v int32) *Writer {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(v))
+	w.buf = append(w.buf, b...)
+	return w
 }
 
-func (w *Writer) WriteString(val string) error {
-	strBytes := []byte(val)
-	if err := w.WriteVarInt(int32(len(strBytes))); err != nil {
-		return err
-	}
-	_, err := w.buf.Write(strBytes)
-	return err
+// Fixed Length Unsigned
+
+func (w *Writer) UByte(v uint8) *Writer {
+	w.buf = append(w.buf, v)
+	return w
 }
 
-func (w *Writer) WriteShort(val int16) error {
-	var b [2]byte
-	binary.BigEndian.PutUint16(b[:], uint16(val))
-	_, err := w.buf.Write(b[:])
-	return err
+func (w *Writer) UShort(v uint16) *Writer {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, v)
+	w.buf = append(w.buf, b...)
+	return w
 }
 
-func (w *Writer) WriteUnsignedShort(val uint16) error {
-	var b [2]byte
-	binary.BigEndian.PutUint16(b[:], val)
-	_, err := w.buf.Write(b[:])
-	return err
+func (w *Writer) UInt(v uint32) *Writer {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, v)
+	w.buf = append(w.buf, b...)
+	return w
 }
 
-func (w *Writer) WriteInt(val int32) error {
-	var b [4]byte
-	binary.BigEndian.PutUint32(b[:], uint32(val))
-	_, err := w.buf.Write(b[:])
-	return err
+// Floating Point
+
+func (w *Writer) Float(v float32) *Writer {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, math.Float32bits(v))
+	w.buf = append(w.buf, b...)
+	return w
 }
 
-func (w *Writer) WriteLong(val int64) error {
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], uint64(val))
-	_, err := w.buf.Write(b[:])
-	return err
+func (w *Writer) Double(v float64) *Writer {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, math.Float64bits(v))
+	w.buf = append(w.buf, b...)
+	return w
 }
 
-func (w *Writer) WriteFloat(val float32) error {
-	var b [4]byte
-	binary.BigEndian.PutUint32(b[:], math.Float32bits(val))
-	_, err := w.buf.Write(b[:])
-	return err
+func (w *Writer) Bytes(v []byte) *Writer {
+	w.buf = append(w.buf, v...)
+	return w
 }
 
-func (w *Writer) WriteDouble(val float64) error {
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], math.Float64bits(val))
-	_, err := w.buf.Write(b[:])
-	return err
+func (w *Writer) Build(packetID int32) []byte {
+	body := NewWriter().VarInt(packetID).Bytes(w.buf).buf
+	return NewWriter().VarInt(int32(len(body))).Bytes(body).buf
 }
 
-func (w *Writer) WriteBool(val bool) error {
-	if val {
-		return w.buf.WriteByte(1)
-	}
-	return w.buf.WriteByte(0)
-}
-
-func (w *Writer) WritePosition(x, y, z int32) error {
-	// Bit-packing untuk koordinat
-	val := (int64(x&0x3FFFFFF) << 38) | (int64(y&0xFFF) << 26) | int64(z&0xFFFFFF)
-	return w.WriteLong(val)
-}
-
-func (w *Writer) WriteBytes(data []byte) error {
-	_, err := w.buf.Write(data)
-	return err
+func (w *Writer) Raw() []byte {
+	return w.buf
 }
